5baa438fd94eb32ff667998b4b9daeaf84da0562
retr0 <retr0lbb@gmail.com>
2023-03-04 00:36:08 -0300

    adding games images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const endPointUrl = 'https://api.rawg.io/api/games';
 const apiKey = 'beddf6dea98848bf94a6a9d5f7f28e48';
 
+
 const queryParams = {
     key: apiKey,
     page:1,
@@ -11,7 +12,17 @@ const url = `${endPointUrl}?${new URLSearchParams(queryParams)}`
 
 fetch(url)
     .then(response => response.json())
-    .then(data =>{
-        console.log(data.results)
-    })
-    .catch(error => console.error(error))
+    .then((data) => {
+        const gameListContainer = document.getElementById('gameList');
+        const games = data.results;
+        games.forEach((game) => {
+          const gameTitle = document.createElement('p');
+          gameTitle.textContent = game.name;
+          gameListContainer.appendChild(gameTitle);
+
+          const gameImage = document.createElement('img');
+          gameImage.src = game.background_image;
+          gameListContainer.appendChild(gameImage);
+        });
+      })
+      .catch((error) => console.error(error));
